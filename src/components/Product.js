33c8102627f5b495bb6ecbfbb363c7b8a048053d
Vimal <vimalkumarak478@gmail.com>
2025-06-24 19:06:08 +0530

    button click add & added vkc

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../CartContext";
 
 const Product = (props) => {
   
   const {cart, setCart } = useContext(CartContext);    // ye App.js file se aa rha cart & setCart , cart ko store krne ke liye
+  const [isAdding, setIsAdding] = useState(false);
   const { product } = props;
 
   // addToCart click functionality
@@ -29,12 +30,18 @@ const Product = (props) => {
     _cart.totalItems += 1;
 
     setCart(_cart);
+
+    // click button color change
+    setIsAdding(true);
+    setTimeout(() => {
+      setIsAdding(false);
+    }, 1000);
   };
 
   return (
     <Link to={`/products/${product.id}`}>
-      <div>
-        <img src={product.image} alt="peproni" />
+      <div className="bg-white rounded-lg shadow-md p-4">
+        <img src={product.image} alt="peproni" className="w-full"/>
         <div className="text-center">
           <h2 className="text-large font-bold py-2">{product.name}</h2>
           <span className="bg-gray-200 py-1 rounded-full text-sm px-4">
@@ -43,8 +50,8 @@ const Product = (props) => {
         </div>
         <div className="flex justify-between items-center mt-4">
           <span>₹ {product.caloriesPerServing}</span>
-          <button onClick={(e) => addToCart(e, product)} className="bg-yellow-500 py-1 px-4 rounded-full font-bold cursor-pointer">
-            ADD
+          <button disabled={isAdding} onClick={(e) => addToCart(e, product)} className={`${ isAdding ? 'bg-green-500' : 'bg-yellow-500' } py-1 px-4 rounded-full font-bold cursor-pointer`}>
+            ADD{isAdding ? 'ED' : ''}
           </button>
         </div>
       </div>
