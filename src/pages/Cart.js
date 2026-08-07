ff599cd5535692aa90548ff0ba400f0eaa554b6a
Vimal Kumar Chaudhary <112014356+Munchunnce@users.noreply.github.com>
2026-08-07 21:18:53 +0530

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,6 +7,7 @@ const Cart = () => {
   const { cart, setCart } = useContext(CartContext);
   const [toast, setToast] = useState(null); //  Toast info
 
+
   
   useEffect(() => {
     if (!cart.items || Object.keys(cart.items).length === 0) {
